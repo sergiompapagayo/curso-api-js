b6799fdbc2ab2eeed26a7ab9bfcc474f524155a5
Sergio Medina Papagayo <sergio.m.papagayo@gmail.com>
2022-09-08 10:59:56 -0500

    TrendingMovies and CateegoriesList have been added

--- src/JS/main.js
+++ src/JS/main.js
@@ -1,1 +1,59 @@
-console.log('Hello World');
+const $trendingPreviewMovieList = null || document.querySelector('.trendingPreview-movieList');
+const $categoriesPreviewMovieList = null || document.querySelector('.categoriesPreview-list');
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  params: {
+    'api_key': API_KEY,
+  }
+});
+
+const getTrendingMoviesPreview = async() => {
+  try {
+    const response = await api.get('/trending/movie/day');
+    const trendingMovies = response.data.results;
+    $trendingPreviewMovieList.innerHTML = ``;
+    trendingMovies.forEach(movie => {
+      $trendingPreviewMovieList.innerHTML += 
+      `
+      <div class="movie-container">
+        <img
+          src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+          class="movie-img"
+          alt="${movie.title}"
+        />
+      </div>
+      `;
+    });
+  } catch(error) {
+    alert(error);
+  }
+}
+
+const getCategoriesList = async() => {
+  try {
+    const response = await api.get('/genre/movie/list');
+    const categories = response.data.genres;
+    console.log(categories);
+    categories.forEach(category => {
+      $categoriesPreviewMovieList.innerHTML += 
+      `
+      <div class="category-container">
+      <h3 id="id${category.id}" class="category-title">${category.name}</h3>
+      </div>
+      `;
+      console.log(`id${category.id}`)
+    });
+  } catch(error) {
+    alert(error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', (e) => {
+  getTrendingMoviesPreview();
+  getCategoriesList();
+});
+
